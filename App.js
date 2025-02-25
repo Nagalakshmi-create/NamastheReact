300be6ff72877c52c5ed0a3307adeb84c5f924ac
Naga Lakshmi Chintala <nagalakshmichintala@jmangroup.com>
2025-02-25 16:11:57 +0530

    Ep.2 Ignite Our App - Completed

--- App.js
+++ App.js
@@ -17,12 +17,14 @@
 */
 
 
-
 // const heading = React.createElement("h1", {id: "heading"}, "Hello world from React!");
+
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent = React.createElement("div", { id: "child" },
     [React.createElement("div", { id: "child1" },
-        [React.createElement("h1", {}, "I am an h1 tag"),
+        [React.createElement("h1", {}, "This is Namaste React"),
         React.createElement("h2", {}, "I am an h2 tag")]
     ), React.createElement("div", { id: "child2" },
         [React.createElement("h1", {}, "I am an h1 tag"),
